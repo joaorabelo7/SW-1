8fef2b1f0212d9b41776b610f2feda8c85d44e18
joaorabelo7 <161508689+joaorabelo7@users.noreply.github.com>
2025-02-14 16:55:10 -0300

    Add files via upload

---
+++ exemplo01.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exemplo 01 - PHP</title>
+</head>
+<body>
+
+
+    <p>Oi sou o Jorge Henrique</p>
+    <?php 
+        echo"<p>Oi sou o Jorge Henrique</p>";
+        $nome = "Jorge Henrique";
+        echo "<p>Oi sou o $nome</p>";
+        echo "<p>Oi sou o ".$nome."</p>";
+    ?>
+</body>
+</html>
