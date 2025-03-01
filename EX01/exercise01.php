aff08cd7a7ed5bb6392636d2e6ba443cdea3d3f1
joaorabelo7 <161508689+joaorabelo7@users.noreply.github.com>
2025-03-01 14:12:00 -0300

    Add files via upload

---
+++ EX01/exercise01.php
@@ -0,0 +1,26 @@
+<?php
+
+$name = "Rabelo";
+$age = 16;
+$wage = 12000;
+
+echo "Hello, what's ur name?<br>";
+
+
+echo "$name<br>";
+
+echo "Hello $name! how old are you?<br>";
+
+echo "$age<br>";
+
+echo "So, you are $age<br>";
+
+echo "How many do you get $name?<br>";
+
+echo "$wage<br>";
+
+echo "$wage! I hope it's by the hour and not by the year <br>";
+
+
+
+?>
