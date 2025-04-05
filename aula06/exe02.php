46d08d7958a3bdeb3b85e412e56b348510dfe4c3
joaorabelo7 <161508689+joaorabelo7@users.noreply.github.com>
2025-04-05 13:32:32 -0300

    Add files via upload

---
+++ aula06/exe02.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<ul>
+<?php
+    
+    for($i=1; $i <=10; $i++){
+        echo"<li> Item $i</li>";
+    }
+
+?>
+</ul>
+    
+</body>
+</html>
